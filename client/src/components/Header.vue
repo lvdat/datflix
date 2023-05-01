b04a159d58378559b946de1851fd3fee8cdc8128
lvdat <lvdat13@gmail.com>
2023-05-01 07:41:16 +0700

    some fix Header CSS

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top main__nav">
         <div class="container">
-        <a class="navbar-brand" href="/"><b><i class="fas fa-dice-d20"></i> DATFLIX</b> </a>
+        <a class="navbar-brand" href="/"><b><i class="fab fa-youtube"></i> DATFLIX</b> </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,7 +21,7 @@
                 <li class="nav-item">
                     <a class="nav-link active" href="/login"> Đăng nhập</a>
                 </li>
-                <button class="btn btn-success btn__signup" type="submit">Đăng ký tham gia</button>
+                <button class="btn btn-success btn__signup" type="submit">Tạo tài khoản </button>
                 <li class="nav-item dropdown">
                     <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fas fa-user-circle"></i> Xin chào,
@@ -41,5 +41,24 @@ export default {
 }
 </script>
 <style>
+.btn__signup {
+    padding: 0px 20px 0px 20px;
+}
 
+.nav-item {
+    margin-right: 15px;
+}
+
+a.nav-link.active:hover {
+    color: rgb(0, 255, 133);
+}
+
+.main__nav {
+    box-shadow: 0px 3px 6px black;
+    color: rgb(232, 231, 228);
+    background-color: rgb(23, 24, 27);
+}
+.navbar-brand {
+    color: rgb(0, 255, 133)!important;
+}
 </style>
