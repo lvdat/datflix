83a097bd4769a9619080cc5ddfcc66eb4d8bdc59
lvdat <lvdat13@gmail.com>
2023-05-01 07:05:41 +0700

    add bootstrap

---
+++ api/models/User.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: Number,
+        default: 0
+    },
+})
+
+export default mongoose.model('User', userSchema)
