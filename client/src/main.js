6391c7eee3a7afd18ee2c9f36b3853457db41cf2
lvdat <lvdat13@gmail.com>
2023-05-01 07:21:15 +0700

    add FontAwesome

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './assets/main.css'
 
 const app = createApp(App)
