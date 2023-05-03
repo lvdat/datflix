8b53f1463e0aac12b2256237ecb1be1edc9554ac
lvdat <lvdat13@gmail.com>
2023-05-03 08:37:24 +0700

    router login for admin

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,11 +4,18 @@ const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
     routes: [{
-        path: '/',
-        name: 'home',
-        component: () =>
-            import ('@/views/HomeView.vue')
-    }, ]
+            path: '/',
+            name: 'home',
+            component: () =>
+                import ('@/views/HomeView.vue')
+        },
+        {
+            path: '/admin/login',
+            name: 'adminLogin',
+            component: () =>
+                import ('@/views/LoginView.vue')
+        },
+    ]
 })
 
 export default router
