db1bb5b889f61d69c395802af0087bb251980ea6
lvdat <lvdat13@gmail.com>
2023-05-03 12:17:51 +0700

    category model

--- api/models/Category.model.js
+++ api/models/Category.model.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose'
+import slugify from 'slugify'
 
 const categorySchema = new mongoose.Schema({
     name: String,
     slug: String,
+    set: (value) => {
+        const processedValue = value.trim().toLowerCase()
+        const slug = slugify(processedValue, { lower: true })
+        return slug
+    }
 })
 
 export default mongoose.model('Category', categorySchema)
