80b3bb4f5a4e1248c67d536ff5620bd2cb9bcdc8
lvdat <lvdat13@gmail.com>
2023-05-03 09:54:30 +0700

    init api service, ans some service

---
+++ client/src/services/api.default.service.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const config = {
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    }
+}
+
+export default (baseURL) => {
+    return axios.create({
+        baseURL,
+        ...config
+    })
+}
+
+// Default API: no require login after fetch!
